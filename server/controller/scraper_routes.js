ca20fe6b4bb6680f9832469ce545a149f71308d8
hassan635 <hassan.a.tariq@gmail.com>
2020-07-31 22:22:53 +1000

    Init browser

--- server/controller/scraper_routes.js
+++ server/controller/scraper_routes.js
@@ -1,5 +1,8 @@
 const router = express.Router()
 const puppeteer = require('puppeteer')
+const browser = await puppeteer.launch({
+    headless: false,
+  });
 
 router.use(express.json())
 router.use(urlencoded({extended: false}))
