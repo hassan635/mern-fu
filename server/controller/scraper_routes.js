166b2fe041b527ee86748a827068389ac0d77cc8
hassan635 <hassan.a.tariq@gmail.com>
2020-08-01 20:16:48 +1000

    Page initiated

--- server/controller/scraper_routes.js
+++ server/controller/scraper_routes.js
@@ -3,10 +3,12 @@ const puppeteer = require('puppeteer')
 const browser = await puppeteer.launch({
     headless: false,
   });
+const page = await browser.newPage();
 
 router.use(express.json())
 router.use(urlencoded({extended: false}))
 
 router.get("scrape", (req, res) => {
+    
     res.send("Scarper initiated")
 })
